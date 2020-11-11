f2a5fd39ed6a2afa533905ce937d3fb5164b78ba
sip <dspirin@me.com>
2020-11-11 14:08:35 -0500

    Method search with Google example

--- src/test/java/newPkg/NewProject.java
+++ src/test/java/newPkg/NewProject.java
@@ -1,6 +1,8 @@
 package newPkg;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,20 +11,27 @@ import org.testng.annotations.Test;
 public class NewProject {
 
     @Test
-    public void sample() {
+    public void search() {
 
         System.setProperty("webdriver.chrome.driver", "./DriverExe/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.facebook.com");
+        driver.get("https://www.google.com");
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        driver.close();
 
-        Assert.assertEquals(1, 1);
+        WebElement searchField = driver.findElement(By.name("q"));
+
+        searchField.sendKeys("Hello World");
+        searchField.submit();
+
+        WebElement newLink = driver.findElements(By.linkText("Videos")).get(0);
+        newLink.click();
+
+        driver.close();
     }
 }
